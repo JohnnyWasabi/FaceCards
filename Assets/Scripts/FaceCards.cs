40ad05ed74960e75407261f21e9c35cbf1b4b659
JohnnyWasabi <john_m_alvarado@yahoo.com>
2017-04-24 10:46:15 -0700

    Cleared debug card limit. Incremented version to 1.5

--- Assets/Scripts/FaceCards.cs
+++ Assets/Scripts/FaceCards.cs
@@ -996,7 +996,7 @@ public class FaceCards : MonoBehaviour {
 			}
 
 			// Version
-			GUI.Label(new Rect(Screen.width-40 - btnWidthSpaced, Screen.height-16, 48, 16), "V 1.4", guiStyleVersion);
+			GUI.Label(new Rect(Screen.width-40 - btnWidthSpaced, Screen.height-16, 48, 16), "V 1.5", guiStyleVersion);
 		}
     }
 }
